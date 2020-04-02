d9e1c52913fd7783dd23cdb5c455a2a161fe3aee
Sebastian Wilczek <sebastian.wilczek@protonmail.com>
2020-04-02 13:22:44 +0200

    Last fix

--- index.php
+++ index.php
@@ -5,43 +5,33 @@
 	
    <body>
       <h2>Enter Username and Password</h2> 
-
          <?php
-
             $msg = '';
-            
             if (isset($_POST['login']) && !empty($_POST['username']) && !empty($_POST['password'])) {
-
                $dbname = $_SERVER['DB_NAME'];
                $dbuser = $_SERVER['DB_USER'];
                $dbpassword = $_SERVER['DB_PASSWORD'];
                $dbhost = $_SERVER['DB_HOST'];
                $dbport = $_SERVER['DB_PORT'];
          		$pdo = new PDO('pgsql:dbname=' . $dbname . ';user=' . $dbuser . ';password=' . $dbpassword . ';host=' . $dbhost . ';port=' . $dbport);
-				
-				$sql = "SELECT username, password
-				FROM users
-				WHERE username = :username
-				AND password = :pass";
-				
-				$sth = $pdo->prepare($sql);
-				$sth->execute(array(':username' => $_POST['username'], ':pass' => $_POST['password']));
-				$result = $sth->fetchAll();
-
-				
-				if (pg_num_rows($result) > 0) {
-               $sql = "SELECT current_database() as name, version() as version";
-               $sth = $pdo->prepare($sql);
-               $sth->execute();
-               $result = $sth->fetch();
-
-				   $msg = 'Correct login, database name is ' . $result[0] . ', version is ' . $result[1];
-				} else {
-				   $msg = 'Wrong username or password';
-				}
+				   $sql = "SELECT username, password
+				   FROM users
+				   WHERE username = :username
+				   AND password = :pass";
+				   $sth = $pdo->prepare($sql);
+				   $sth->execute(array(':username' => $_POST['username'], ':pass' => $_POST['password']));
+				   $result = $sth->fetchAll();
+				   if (count($result) > 0) {
+                  $sql = "SELECT current_database() as name, version() as version";
+                  $sth = $pdo->prepare($sql);
+                  $sth->execute();
+                  $result = $sth->fetch();
+				      $msg = 'Correct login, database name is ' . $result[0] . ', version is ' . $result[1];
+				   } else {
+				      $msg = 'Wrong username or password';
+				   }
             }
          ?>
-      
       <div>
          <form role = "form" action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method = "post">
             <h4><?php echo $msg; ?></h4>
